762ac88f714ccf4e268378ea062ba11b966be5a7
Charles Farnese <charles@farnese.dev>
2022-10-14 16:27:57 +0100

    test

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const e = React.createElement;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>{this.props.conversation-id}</div>
+    );
+  }
+}
+
+class LegacyPlugin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {data: null, conversation_id: 'unknown'};
+  }
+
+  componentDidMount() {
+    window.Front.on("conversation", (data) => {
+      console.log("Event data --> ", data);
+      this.setState({data: data, conversation_id: data.conversation.id});
+    });
+  }
+
+  render() {
+    return e(
+      <App/>,
+      {conversation_id: this.state.conversation_id}
+    );
+    
+  }
+}
+
+const domContainer = document.querySelector('#app');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LegacyPlugin));
